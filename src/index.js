43a3d845db253a3ba0f24bb26e24bd7945bf443b
sagar sharma <sagarkush2003@gmail.com>
2023-08-11 14:24:42 +0530

    set up basic dotenv

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 //repository give method using these method we can actully use those models
 //models->how tables or database will look like
+
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+const setupAndStartServer = async () => {
+    const app = express();
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+    })
+
+}
+setupAndStartServer();
