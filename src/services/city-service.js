3511ac4349bc8a71e4b40608f70e727e8bba32c1
sagar sharma <sagarkush2003@gmail.com>
2023-08-11 23:55:09 +0530

    Added service layer

---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor() {
+        this.CityRepository = new CityRepository();
+    }
+    async createCity(data) {
+        try {
+           const city = await this.CityRepository.createCity(data); 
+           return city;
+        } catch (error) {
+            console.log("error in service layer");
+            throw{error};
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const city = await this.CityRepository.deleteCity(cityId);
+            return city;    
+        } catch (error) {
+            console.log("error in service layer");
+            throw{error};
+        }
+    }
+
+    async updateCity(cityId,data) {
+        try {
+          const city = await this.CityRepository.updateCity(cityId,data);
+          return city;  
+        } catch (error) {
+            console.log("error in service layer");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.CityRepository.getCity(cityId);
+            return city;  
+        } catch (error) {
+            console.log("error in service layer");
+            throw{error};
+        }
+    }
+}
+
+module.exports = CityService;
